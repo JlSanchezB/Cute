77d7c9244b17a2c5c95816c0f517e763347d1ca5
Pocket <pocket.chanka@gmail.com>
2018-12-02 20:33:34 +0000

    WIP render pass system

--- engine/render/render_resource.cpp
+++ engine/render/render_resource.cpp
@@ -27,11 +27,53 @@ namespace
 	}
 
 	//Conversion tables
-	const std::pair<const char*, display::Access> g_display_access_converison[] =
+	const std::pair<const char*, display::Access> g_display_access_conversion[] =
 	{
 		{"Static", display::Access::Static},
 		{"Dynamic", display::Access::Dynamic}
 	};
+
+	const std::pair<const char*, display::RootSignatureParameterType> g_display_root_signature_parameter_type_conversion[] =
+	{
+		{"Constantbuffer", display::RootSignatureParameterType::ConstantBuffer},
+		{"Constants", display::RootSignatureParameterType::Constants},
+		{"DescriptorTable", display::RootSignatureParameterType::DescriptorTable},
+		{"ShaderResource", display::RootSignatureParameterType::ShaderResource},
+		{"UnorderedAccessBuffer", display::RootSignatureParameterType::UnorderAccessBuffer}
+	};
+
+	const std::pair<const char*, display::DescriptorTableParameterType> g_display_descriptor_table_parameter_type_conversion[] =
+	{
+		{"Constantbuffer", display::DescriptorTableParameterType::ConstantBuffer},
+		{"UnorderAccessBuffer", display::DescriptorTableParameterType::UnorderAccessBuffer},
+		{"ShaderResource", display::DescriptorTableParameterType::ShaderResource},
+		{"Sampler", display::DescriptorTableParameterType::Sampler}
+	};
+
+	const std::pair<const char*, display::ShaderVisibility> g_display_shader_visibility_conversion[] =
+	{
+		{"All", display::ShaderVisibility::All},
+		{"Domain", display::ShaderVisibility::Domain},
+		{"Geometry", display::ShaderVisibility::Geometry},
+		{"Hull", display::ShaderVisibility::Hull},
+		{"Pixel", display::ShaderVisibility::Pixel},
+		{"Vertex", display::ShaderVisibility::Vertex}
+	};
+
+	const std::pair<const char*, display::Filter> g_display_filter_conversion[] =
+	{
+		{"Point", display::Filter::Point},
+		{"Linear", display::Filter::Linear},
+		{"Anisotropic", display::Filter::Anisotropic}
+	};
+
+	const std::pair<const char*, display::TextureAddressMode> g_display_texture_address_mode[] =
+	{
+		{"Wrap", display::TextureAddressMode::Wrap},
+		{"Mirror", display::TextureAddressMode::Mirror},
+		{"Clamp", display::TextureAddressMode::Clamp}
+	};
+	
 }
 
 void render::BoolResource::Load(LoadContext& load_context)
@@ -48,7 +90,7 @@ void render::BoolResource::Load(LoadContext& load_context)
 	}
 	else
 	{
-		AddError(load_context, "BoolResource doesn't have a 'True' or 'False' value");
+		AddError(load_context, "BoolResource <%s> doesn't have a 'True' or 'False' value", load_context.name);
 	}
 }
 
@@ -89,7 +131,7 @@ void render::ConstantBufferResource::Load(LoadContext& load_context)
 	}
 
 	//Read access, by default static
-	QueryEnumAttribute(xml_element, "access", &constant_buffer_desc.access, g_display_access_converison);
+	QueryEnumAttribute(xml_element, "access", &constant_buffer_desc.access, g_display_access_conversion);
 
 	//Create constant buffer
 	m_constant_buffer_handle = display::CreateConstantBuffer(load_context.device, constant_buffer_desc, load_context.name);
@@ -102,4 +144,41 @@ void render::ConstantBufferResource::Load(LoadContext& load_context)
 
 void render::RootSignatureResource::Load(LoadContext& load_context)
 {
+	display::RootSignatureDesc root_signature_desc;
+
+	auto xml_element = load_context.current_xml_element->FirstChildElement();
+
+	while (xml_element)
+	{
+		if (strcmp(xml_element->Name(), "RootParam") == 0)
+		{
+			//New root param
+			auto& current_root_param = root_signature_desc.root_parameters[root_signature_desc.num_root_parameters++];
+
+			if (!QueryEnumAttribute(xml_element, "type", &current_root_param.type, g_display_root_signature_parameter_type_conversion))
+			{
+				AddError(load_context, "Root signature param type is not valid for <%> root signature", load_context.name);
+				return;
+			}
+
+			if (!QueryEnumAttribute(xml_element, "visibility", &current_root_param.visibility, g_display_shader_visibility_conversion))
+			{
+				AddError(load_context, "Root signature param visibility is not valid for <%> root signature", load_context.name);
+				return;
+			}
+		}
+		else if (strcmp(xml_element->Name(), "StaticSample") == 0)
+		{
+
+		}
+		else
+		{
+			AddError(load_context, "Invalid xml element found <%s> in root signature <%s>", xml_element->Name(), load_context.name);
+			return;
+		}
+
+		xml_element = xml_element->NextSiblingElement();
+	}
+
+	//Create root signature
 }
