7de39a647c24483d2b99cb7127f303cf3a620e5a
Pocket <pocket.chanka@gmail.com>
2018-12-03 22:38:41 +0000

    Render passes create root signature resource

--- engine/render/render_resource.cpp
+++ engine/render/render_resource.cpp
@@ -137,49 +137,74 @@ void render::RootSignatureResource::Load(LoadContext& load_context)
 			//New root param
 			auto& current_root_param = root_signature_desc.root_parameters[root_signature_desc.num_root_parameters++];
 
-			QueryTableAttribute(load_context, xml_element_root, "type", current_root_param.type, g_display_root_signature_parameter_type_conversion, AttributeType::NonOptional);
-			QueryTableAttribute(load_context, xml_element_root, "visibility", current_root_param.visibility, g_display_shader_visibility_conversion, AttributeType::Optional);
-		
-			if (current_root_param.type == display::RootSignatureParameterType::DescriptorTable)
+			if (root_signature_desc.num_root_parameters < display::kMaxNumRootParameters)
 			{
-				//Read ranges
-				auto xml_element_range = xml_element_root->FirstChildElement();
+				QueryTableAttribute(load_context, xml_element_root, "type", current_root_param.type, g_display_root_signature_parameter_type_conversion, AttributeType::NonOptional);
+				QueryTableAttribute(load_context, xml_element_root, "visibility", current_root_param.visibility, g_display_shader_visibility_conversion, AttributeType::Optional);
 
-				while (xml_element_range)
+				if (current_root_param.type == display::RootSignatureParameterType::DescriptorTable)
 				{
-					//Add range
-					if (strcmp(xml_element_range->Name(), "RootParam") == 0)
+					//Read ranges
+					auto xml_element_range = xml_element_root->FirstChildElement();
+
+					while (xml_element_range)
 					{
-						if (current_root_param.table.num_ranges == display::RootSignatureTable::kNumMaxRanges)
+						//Add range
+						if (strcmp(xml_element_range->Name(), "RootParam") == 0)
 						{
-							AddError(load_context, "Max number of range reach in root signature <%s>", load_context.name);
+							if (current_root_param.table.num_ranges == display::RootSignatureTable::kNumMaxRanges)
+							{
+								AddError(load_context, "Max number of range reach in root signature <%s>", load_context.name);
+								return;
+							}
+							else
+							{
+								auto& range = current_root_param.table.range[current_root_param.table.num_ranges++];
+
+								QueryTableAttribute(load_context, xml_element_range, "type", range.type, g_display_descriptor_table_parameter_type_conversion, AttributeType::NonOptional);
+								QueryAttribute(load_context, xml_element_range, "base_shader_register", range.base_shader_register, AttributeType::NonOptional);
+								QueryAttribute(load_context, xml_element_range, "size", range.size, AttributeType::NonOptional);
+							}
 						}
 						else
 						{
-							auto& range = current_root_param.table.range[current_root_param.table.num_ranges++];
-
-							QueryTableAttribute(load_context, xml_element_range, "type", range.type, g_display_descriptor_table_parameter_type_conversion, AttributeType::NonOptional);
-							QueryAttribute(load_context, xml_element_range, "base_shader_register", range.base_shader_register, AttributeType::NonOptional);
-							QueryAttribute(load_context, xml_element_range, "size", range.size, AttributeType::NonOptional);
+							AddError(load_context, "Expected Range element inside root signature <%s>", load_context.name);
 						}
+						xml_element_range = xml_element_range->NextSiblingElement();
 					}
-					else
-					{
-						AddError(load_context, "Expected Range element inside root signature <%s>", load_context.name);
-					}
-					xml_element_range = xml_element_range->NextSiblingElement();
+				}
+				else
+				{
+					//Read basic root constant
+					QueryAttribute(load_context, xml_element_root, "shader_register", current_root_param.root_param.shader_register, AttributeType::NonOptional);
+					QueryAttribute(load_context, xml_element_root, "num_constants", current_root_param.root_param.num_constants, AttributeType::Optional);
 				}
 			}
 			else
 			{
-				//Read basic root constant
-				QueryAttribute(load_context, xml_element_root, "shader_register", current_root_param.root_param.shader_register, AttributeType::NonOptional);
-				QueryAttribute(load_context, xml_element_root, "num_constants", current_root_param.root_param.num_constants, AttributeType::Optional);
+				AddError(load_context, "Max number of root parameters reach in root signature <%s>", load_context.name);
+				return;
 			}
 		}
 		else if (strcmp(xml_element_root->Name(), "StaticSample") == 0)
 		{
-
+			//New static sampler
+			auto& current_static_sampler = root_signature_desc.static_samplers[root_signature_desc.num_static_samplers++];
+			if (root_signature_desc.num_static_samplers <= display::kMaxNumStaticSamplers)
+			{
+				QueryAttribute(load_context, xml_element_root, "shader_register", current_static_sampler.shader_register, AttributeType::NonOptional);
+				QueryTableAttribute(load_context, xml_element_root, "visibility", current_static_sampler.visibility, g_display_shader_visibility_conversion, AttributeType::Optional);
+				
+				QueryTableAttribute(load_context, xml_element_root, "filter", current_static_sampler.filter, g_display_filter_conversion, AttributeType::Optional);
+				QueryTableAttribute(load_context, xml_element_root, "address_u", current_static_sampler.address_u, g_display_texture_address_mode, AttributeType::Optional);
+				QueryTableAttribute(load_context, xml_element_root, "address_v", current_static_sampler.address_v, g_display_texture_address_mode, AttributeType::Optional);
+				QueryTableAttribute(load_context, xml_element_root, "address_w", current_static_sampler.address_w, g_display_texture_address_mode, AttributeType::Optional);
+			}
+			else
+			{
+				AddError(load_context, "Max number of static sampler reach in root signature <%s>", load_context.name);
+				return;
+			}
 		}
 		else
 		{
@@ -190,4 +215,10 @@ void render::RootSignatureResource::Load(LoadContext& load_context)
 	}
 
 	//Create root signature
+	GetHandle() = display::CreateRootSignature(load_context.device, root_signature_desc, load_context.name);
+
+	if (!GetHandle().IsValid())
+	{
+		AddError(load_context, "Error creating root signature <%s>, display error <%>", load_context.name, display::GetLastErrorMessage(load_context.device));
+	}
 }
