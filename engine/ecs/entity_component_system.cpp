dabf11d115627394cb07ae1f2928cea5ed520296
Pocket <pocket.chanka@gmail.com>
2019-01-19 21:58:51 +0000

    Started the structure of the Database Defining the Component declarations

--- engine/ecs/entity_component_system.cpp
+++ engine/ecs/entity_component_system.cpp
@@ -2,6 +2,14 @@
 
 namespace ecs
 {
+	//Database
+	//Our Database will have a list of instance types
+	//Instance types depends of the combination of components added to a instance
+	//Each instance type will have a size and a set of memory blocks with all compoments memory
+	struct Database
+	{
+	};
+
 	Database* CreateDatabase(const DatabaseDesc & database_desc)
 	{
 		return nullptr;
--- engine/ecs/entity_component_system.h
+++ engine/ecs/entity_component_system.h
@@ -10,11 +10,28 @@ namespace ecs
 {
 	struct Database;
 
-	//Base class for all components
-	//It will not add data per object, just containts information as the component index in the database
+	//Base class for all components, Component : ecs::Component<Component>
+	//It will not add data per object, just containts information as the component index in the database, using CRTP
+	template<typename COMPONENT>
 	struct Component
 	{
 		inline static size_t s_component_index = static_cast<size_t>(-1);
+
+		//Static functions with copy and move operators
+		static void Copy(void* ptr_a, void* ptr_b)
+		{
+			COMPONENT& a = *ptr_a;
+			COMPONENT& b = *ptr_b;
+			a = b;
+		}
+
+		static void Move(void* ptr_a, void* ptr_b)
+		{
+			COMPONENT& a = *ptr_a;
+			COMPONENT& b = *ptr_b;
+			a = std::move(b);
+		}
+
 	};
 
 	struct ComponentDesc
@@ -22,14 +39,23 @@ namespace ecs
 		size_t size;
 		size_t align;
 
+		//Copy operator
+		void(*copy_operator)(void*, void*);
+		//Move operator
+		void(*move_operator)(void*, void*);
+
 		//Capture the properties of the component
 		template<typename COMPONENT>
 		void Init()
 		{
 			size = sizeof(COMPONENT);
 			align = alignof(COMPONENT);
+
+			copy_operator = Component<COMPONENT>::Copy;
+			move_operator = Component<COMPONENT>::Move;
 		}
 	};
+
 	struct DatabaseDesc
 	{
 		//List of components
@@ -38,14 +64,15 @@ namespace ecs
 		template<typename COMPONENT>
 		void Add()
 		{
+			static_assert(std::is_convertible<COMPONENT*, Component*>::value, "Component class has to derived from ecs::Component");
+			static_assert(Component<COMPONENT>::s_component_index == static_cast<size_t>(-1), "Component class can be used only once and only in one Database");
+
 			ComponentDesc component_desc;
 			component_desc.Init<COMPONENT>();
 			components.push_back(component_desc);
 
 			//Set the index to the component index, so it can be accessed faster
-			static_assert(std::is_convertible<COMPONENT*, Component*>::value, "Component class has to derived from ecs::Component");
-			static_assert(COMPONENT::s_component_index == static_cast<size_t>(-1), "Component class can be used only in one Database");
-			COMPONENT::s_component_index = components.size() - 1;
+			Component<COMPONENT>::s_component_index = components.size() - 1;
 		}
 	};
 
