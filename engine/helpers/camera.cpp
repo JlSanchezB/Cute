48b728968909585d3f627c80a8e7e5ec71b8144d
Pocket <pocket.chanka@gmail.com>
2021-02-19 13:05:13 +0000

    Fixes in the camara, divide it to FlyCamera, Camara and Frustum. Change left/right sticks.

--- engine/helpers/camera.cpp
+++ engine/helpers/camera.cpp
@@ -10,17 +10,33 @@
 #include <ext/glm/gtx/rotate_vector.hpp>
 #include <ext/glm/gtx/euler_angles.hpp>
 
+//https://gist.github.com/podgorskiy/e698d18879588ada9014768e3e82a644
 
 namespace helpers
 {
+	template<FlyCamera::Planes i, FlyCamera::Planes j>
+	struct ij2k
+	{
+		enum { k = i * (9 - i) / 2 + j - 1 };
+	};
+
+	template<FlyCamera::Planes a, FlyCamera::Planes b, FlyCamera::Planes c>
+	inline glm::vec3 intersection(const glm::vec4* planes, const glm::vec3* crosses)
+	{
+		float D = glm::dot(glm::vec3(planes[a]), crosses[ij2k<b, c>::k]);
+		glm::vec3 res = glm::mat3(crosses[ij2k<b, c>::k], -crosses[ij2k<a, c>::k], crosses[ij2k<a, b>::k]) *
+			glm::vec3(planes[a].w, planes[b].w, planes[c].w);
+		return res * (-1.0f / D);
+	}
+
 	//Process input and update the position
 	void FlyCamera::Update(platform::Game* game, float ellapsed_time)
 	{
 		//Read inputs
-		float forward_input = game->GetInputSlotValue(platform::InputSlotValue::ControllerThumbRightY);
+		float forward_input = game->GetInputSlotValue(platform::InputSlotValue::ControllerThumbLeftY);
 		if (game->GetInputSlotState(platform::InputSlotState::Up) || game->GetInputSlotState(platform::InputSlotState::Key_W)) forward_input += 1.f;
 		if (game->GetInputSlotState(platform::InputSlotState::Down) || game->GetInputSlotState(platform::InputSlotState::Key_S)) forward_input -= 1.f;
-		float side_input = game->GetInputSlotValue(platform::InputSlotValue::ControllerThumbRightX);
+		float side_input = game->GetInputSlotValue(platform::InputSlotValue::ControllerThumbLeftX);
 		if (game->GetInputSlotState(platform::InputSlotState::Right) || game->GetInputSlotState(platform::InputSlotState::Key_D)) side_input += 1.f;
 		if (game->GetInputSlotState(platform::InputSlotState::Left) || game->GetInputSlotState(platform::InputSlotState::Key_A)) side_input -= 1.f;
 		float up_input = (game->GetInputSlotValue(platform::InputSlotValue::ControllerRightTrigger) - game->GetInputSlotValue(platform::InputSlotValue::ControllerLeftTrigger));
@@ -38,8 +54,8 @@ namespace helpers
 		up_input = glm::clamp(up_input, -1.f, 1.f);
 
 		glm::vec2 rotation_input;
-		rotation_input.x = game->GetInputSlotValue(platform::InputSlotValue::ControllerThumbLeftX);
-		rotation_input.y = game->GetInputSlotValue(platform::InputSlotValue::ControllerThumbLeftY);
+		rotation_input.x = game->GetInputSlotValue(platform::InputSlotValue::ControllerThumbRightX);
+		rotation_input.y = game->GetInputSlotValue(platform::InputSlotValue::ControllerThumbRightY);
 		if (game->GetInputSlotState(platform::InputSlotState::LeftMouseButton))
 		{
 			rotation_input.x += game->GetInputSlotValue(platform::InputSlotValue::MouseRelativePositionX) * m_mouse_rotate_factor;
@@ -82,23 +98,69 @@ namespace helpers
 		if (m_rotation.y > glm::radians(85.f)) m_rotation.y = glm::radians(85.f);
 		if (m_rotation.y < glm::radians(-85.f)) m_rotation.y = glm::radians(-85.f);
 
-		CalculateMatrices();
+		UpdateInternalData();
 	}
 
-	void FlyCamera::CalculateMatrices()
+	void Camera::UpdateInternalData()
 	{
 		//Calculate view to world
 		glm::mat3x3 rot = glm::rotate(m_rotation.y, glm::vec3(1.f, 0.f, 0.f)) * glm::rotate(m_rotation.x, glm::vec3(0.f, 0.f, 1.f));
-		m_world_to_view_matrix = glm::lookAt(m_position, m_position + glm::vec3(0.f, 1.f, 0.f) * rot, m_up_vector);
+		glm::mat4x4 world_to_view_matrix = glm::lookAt(m_position, m_position + glm::vec3(0.f, 1.f, 0.f) * rot, m_up_vector);
 
 		//Calculate projection matrix
-		m_projection_matrix = glm::perspective(m_fov_y, m_aspect_ratio, m_near, m_far);
+		glm::mat4x4 projection_matrix = glm::perspective(m_fov_y, m_aspect_ratio, m_near, m_far);
+
+		UpdateFrustum(world_to_view_matrix, projection_matrix);
+	}
+	
+	void Frustum::UpdateFrustum(const glm::mat4x4& world_to_view_matrix, const glm::mat4x4& projection_matrix)
+	{
+		m_world_to_view_matrix = world_to_view_matrix;
+		m_projection_matrix = projection_matrix;
 
 		//Calculate view projection
 		m_view_projection_matrix = m_projection_matrix * m_world_to_view_matrix;
+
+		//Calculate frustum planes
+		glm::mat4x4 frustum_matrix = glm::transpose(m_view_projection_matrix);
+
+		m_planes[Planes::Left] = frustum_matrix[3] + frustum_matrix[0];
+		m_planes[Planes::Right] = frustum_matrix[3] - frustum_matrix[0];
+		m_planes[Planes::Bottom] = frustum_matrix[3] + frustum_matrix[1];
+		m_planes[Planes::Top] = frustum_matrix[3] - frustum_matrix[1];
+		m_planes[Planes::Near] = frustum_matrix[3] + frustum_matrix[2];
+		m_planes[Planes::Far] = frustum_matrix[3] - frustum_matrix[2];
+
+		//Calculate points
+		glm::vec3 crosses[Combinations] = {
+			glm::cross(glm::vec3(m_planes[Left]),   glm::vec3(m_planes[Right])),
+			glm::cross(glm::vec3(m_planes[Left]),   glm::vec3(m_planes[Bottom])),
+			glm::cross(glm::vec3(m_planes[Left]),   glm::vec3(m_planes[Top])),
+			glm::cross(glm::vec3(m_planes[Left]),   glm::vec3(m_planes[Near])),
+			glm::cross(glm::vec3(m_planes[Left]),   glm::vec3(m_planes[Far])),
+			glm::cross(glm::vec3(m_planes[Right]),  glm::vec3(m_planes[Bottom])),
+			glm::cross(glm::vec3(m_planes[Right]),  glm::vec3(m_planes[Top])),
+			glm::cross(glm::vec3(m_planes[Right]),  glm::vec3(m_planes[Near])),
+			glm::cross(glm::vec3(m_planes[Right]),  glm::vec3(m_planes[Far])),
+			glm::cross(glm::vec3(m_planes[Bottom]), glm::vec3(m_planes[Top])),
+			glm::cross(glm::vec3(m_planes[Bottom]), glm::vec3(m_planes[Near])),
+			glm::cross(glm::vec3(m_planes[Bottom]), glm::vec3(m_planes[Far])),
+			glm::cross(glm::vec3(m_planes[Top]),    glm::vec3(m_planes[Near])),
+			glm::cross(glm::vec3(m_planes[Top]),    glm::vec3(m_planes[Far])),
+			glm::cross(glm::vec3(m_planes[Near]),   glm::vec3(m_planes[Far]))
+		};
+
+		m_points[0] = intersection<Left, Bottom, Near>(m_planes, crosses);
+		m_points[1] = intersection<Left, Top, Near>(m_planes, crosses);
+		m_points[2] = intersection<Right, Bottom, Near>(m_planes, crosses);
+		m_points[3] = intersection<Right, Top, Near>(m_planes, crosses);
+		m_points[4] = intersection<Left, Bottom, Far>(m_planes, crosses);
+		m_points[5] = intersection<Left, Top, Far>(m_planes, crosses);
+		m_points[6] = intersection<Right, Bottom, Far>(m_planes, crosses);
+		m_points[7] = intersection<Right, Top, Far>(m_planes, crosses);
 	}
 
-	glm::mat4x4 FlyCamera::GetViewProjectionMatrix() const
+	glm::mat4x4 Frustum::GetViewProjectionMatrix() const
 	{
 		return m_view_projection_matrix;
 	}	
--- engine/helpers/camera.h
+++ engine/helpers/camera.h
@@ -9,6 +9,7 @@
 #include <ext/glm/vec2.hpp>
 #include <ext/glm/mat4x4.hpp>
 #include <ext/glm/gtc/constants.hpp>
+#include <array>
 
 namespace platform
 {
@@ -17,30 +18,71 @@ namespace platform
 
 namespace helpers
 {
-	class FlyCamera
+	class Frustum
 	{
 	public:
-		//Process input and update the position
-		void Update(platform::Game* game, float ellapsed_time);
-
-		void CalculateMatrices();
+		enum Planes
+		{
+			Left = 0,
+			Right,
+			Bottom,
+			Top,
+			Near,
+			Far,
+			Count,
+			Combinations = Count * (Count - 1) / 2
+		};
 
 		glm::mat4x4 GetViewProjectionMatrix() const;
+		const glm::vec4* GetPlanes() const { return m_planes; };
+		const glm::vec3* GetPoints() const  { return m_points; };
+
+	protected:
+
+		void UpdateFrustum(const glm::mat4x4& world_to_view_matrix, const glm::mat4x4& projection_matrix);
 
 	private:
-
-		//State
-		glm::vec3 m_position = glm::vec3(0.f, 0.f, 0.f);
-		glm::vec2 m_rotation = glm::vec3(0.f);
-		glm::vec3 m_move_speed = glm::vec3(0.f, 0.f, 0.f);
-		glm::vec2 m_rotation_speed = glm::vec2(0.f, 0.f);
-		glm::vec3 m_up_vector = glm::vec3(0.f, 0.f, 1.f);
-
 		//Matrices
 		glm::mat4x4 m_world_to_view_matrix;
 		glm::mat4x4 m_projection_matrix;
 		glm::mat4x4 m_view_projection_matrix;
 
+		//Frustum planes
+		glm::vec4   m_planes[Planes::Count];
+
+		//Frustum points
+		glm::vec3   m_points[8];
+	};
+
+	class Camera : public Frustum
+	{
+	protected:
+
+		void UpdateInternalData();
+
+		//Setup
+		glm::vec3 m_position = glm::vec3(0.f, 0.f, 0.f);
+		glm::vec2 m_rotation = glm::vec3(0.f);
+		glm::vec3 m_up_vector = glm::vec3(0.f, 0.f, 1.f);
+		float m_fov_y = glm::half_pi<float>();
+		float m_aspect_ratio = 1.f;
+		float m_far = 10000.f;
+		float m_near = 0.1f;
+	
+	private:
+		
+	};
+
+	class FlyCamera : public Camera
+	{
+	public:
+		//Process input and update the position
+		void Update(platform::Game* game, float ellapsed_time);
+
+	private:
+		//State
+		glm::vec3 m_move_speed = glm::vec3(0.f, 0.f, 0.f);
+		glm::vec2 m_rotation_speed = glm::vec2(0.f, 0.f);
 
 		//Setup
 		float m_mouse_rotate_factor = 2.5f;
@@ -48,10 +90,6 @@ namespace helpers
 		float m_damp_factor = 5.0f;
 		float m_move_factor = 40.0f;
 		float m_rotation_factor = 20.f;
-		float m_fov_y = glm::half_pi<float>();
-		float m_aspect_ratio = 1.f;
-		float m_far = 10000.f;
-		float m_near = 0.1f;
 	};
 }
 
