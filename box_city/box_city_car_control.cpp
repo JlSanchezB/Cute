ed633ba0fe434da7250d885de92c87f5fe10132f
Pocket <pocket.chanka@gmail.com>
2022-12-02 20:34:45 +0000

    Change camera distances and fov

--- box_city/box_city_car_control.cpp
+++ box_city/box_city_car_control.cpp
@@ -38,9 +38,9 @@ CONTROL_VARIABLE(float, c_car_friction_linear_force, 0.f, 10.f, 1.8f, "Car", "Li
 CONTROL_VARIABLE(float, c_car_friction_angular_force, 0.f, 10.f, 1.8f, "Car", "Angular Friction Force");
 
 
-CONTROL_VARIABLE(float, c_car_camera_distance, 0.f, 100.f, 28.f, "Car", "Camera Distance");
-CONTROL_VARIABLE(float, c_car_camera_up_offset, 0.f, 100.f, 6.f, "Car", "Camera Up Offset");
-CONTROL_VARIABLE(float, c_car_camera_fov, 60.f, 180.f, 90.f, "Car", "Camera Fov");
+CONTROL_VARIABLE(float, c_car_camera_distance, 0.f, 100.f, 11.f, "Car", "Camera Distance");
+CONTROL_VARIABLE(float, c_car_camera_up_offset, 0.f, 100.f, 4.f, "Car", "Camera Up Offset");
+CONTROL_VARIABLE(float, c_car_camera_fov, 60.f, 180.f, 100.f, "Car", "Camera Fov");
 
 namespace BoxCityCarControl
 {
