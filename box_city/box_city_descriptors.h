e219c22dc5027781a768783291062692a1db8a5f
Pocket <pocket.chanka@gmail.com>
2023-02-16 21:52:50 +0000

    Improved building sizes

--- box_city/box_city_descriptors.h
+++ box_city/box_city_descriptors.h
@@ -28,11 +28,11 @@ namespace BoxCityTileSystem
 
 	const ZoneDescriptor kZoneDescriptors[kNumZoneDescriptors] =
 	{
-		{50.f, 150.f, -glm::half_pi<float>() * 0.2f, glm::half_pi<float>() * 0.2f, 20.0f, 30.0f, 0.f, 50.f, 0.3f, 1.f, 0.f, 30.f, 10.f, 350, 2.f, 5.f, 15.f, 16, 50.f},
-		{50.f, 120.f, -glm::half_pi<float>() * 0.1f, glm::half_pi<float>() * 0.1f, 30.0f, 40.0f, 0.f, 150.f, 0.5f, 1.f, 0.f, 30.f, 10.f, 350, 2.f, 5.f, 15.f, 16, 40.f},
-		{40.f, 100.f, -glm::half_pi<float>() * 0.25f, glm::half_pi<float>() * 0.25f, 15.0f, 25.0f, 0.f, 70.f, 0.35f, 1.f, 0.f, 30.f, 10.f, 350, 2.f, 5.f, 15.f, 16, 45.f},
-		{30.f, 170.f, -glm::half_pi<float>() * 0.15f, glm::half_pi<float>() * 0.15f, 20.0f, 40.0f, 0.f, 50.f, 0.25f, 1.f, 0.f, 30.f, 10.f, 350, 2.f, 5.f, 15.f, 16, 40.f},
-		{55.f, 200.f, -glm::half_pi<float>() * 0.2f, glm::half_pi<float>() * 0.2f, 15.0f, 30.0f, 0.f, 50.f, 0.4f, 1.f, 0.f, 30.f, 10.f, 350, 2.f, 5.f, 15.f, 16, 30.f},
-		{60.f, 150.f, -glm::half_pi<float>() * 0.15f, glm::half_pi<float>() * 0.15f, 20.0f, 40.0f, 0.f, 50.f, 0.1f, 1.f, 0.f, 30.f, 10.f, 350, 2.f, 5.f, 15.f, 16, 60.f}
+		{50.f, 150.f, -glm::half_pi<float>() * 0.2f, glm::half_pi<float>() * 0.2f, 15.0f, 25.0f, 0.f, 50.f, 0.3f, 1.f, 0.f, 30.f, 10.f, 350, 2.f, 5.f, 15.f, 16, 50.f},
+		{50.f, 120.f, -glm::half_pi<float>() * 0.1f, glm::half_pi<float>() * 0.1f, 25.0f, 35.0f, 0.f, 150.f, 0.5f, 1.f, 0.f, 30.f, 10.f, 350, 2.f, 5.f, 15.f, 16, 40.f},
+		{40.f, 100.f, -glm::half_pi<float>() * 0.25f, glm::half_pi<float>() * 0.25f, 10.0f, 20.0f, 0.f, 70.f, 0.35f, 1.f, 0.f, 30.f, 10.f, 350, 2.f, 5.f, 15.f, 16, 45.f},
+		{30.f, 170.f, -glm::half_pi<float>() * 0.15f, glm::half_pi<float>() * 0.15f, 15.0f, 35.0f, 0.f, 50.f, 0.25f, 1.f, 0.f, 30.f, 10.f, 350, 2.f, 5.f, 15.f, 16, 40.f},
+		{55.f, 200.f, -glm::half_pi<float>() * 0.2f, glm::half_pi<float>() * 0.2f, 10.0f, 25.0f, 0.f, 50.f, 0.4f, 1.f, 0.f, 30.f, 10.f, 350, 2.f, 5.f, 15.f, 16, 30.f},
+		{60.f, 150.f, -glm::half_pi<float>() * 0.15f, glm::half_pi<float>() * 0.15f, 15.0f, 35.0f, 0.f, 50.f, 0.1f, 1.f, 0.f, 30.f, 10.f, 350, 2.f, 5.f, 15.f, 16, 60.f}
 	};
 }
