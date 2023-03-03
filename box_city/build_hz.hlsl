8be0aff1c4cc06b169231e3b56ab6964b1a5180e
Pocket <pocket.chanka@gmail.com>
2023-03-03 18:02:36 +0000

    Improved HiZ shader

--- box_city/build_hz.hlsl
+++ box_city/build_hz.hlsl
@@ -1,9 +1,9 @@
 
 
 //Indirect parameters for draw call
-RWTexture2D<float> hz_texture : register(u0);
+globallycoherent RWTexture2D<float> hz_texture : register(u0);
 Texture2D<float> scene_depth : register(t0);
-RWStructuredBuffer<uint> atomic_buffer : register(u1);
+globallycoherent RWStructuredBuffer<uint> atomic_buffer : register(u1);
 
 groupshared uint min_z_value_mip[8*8 + 4*4 + 2*2 + 1];
 static const uint mip0_offset = 0;
@@ -28,8 +28,6 @@ void build_hz(uint3 group : SV_GroupID, uint3 group_thread_id : SV_GroupThreadID
 		min_z_value_mip[thread_index] = asuint(1.f);
 	}
 
-	GroupMemoryBarrierWithGroupSync();
-
 	uint2 quad = uint2(thread_index % 16, thread_index / 16);
 
 	{
@@ -39,10 +37,16 @@ void build_hz(uint3 group : SV_GroupID, uint3 group_thread_id : SV_GroupThreadID
 		float min_value = 1.f;
 
 		uint2 texture_access;
-		texture_access = group.xy * 64 + quad * 4;
+		texture_access = (group.xy * 64 + quad * 4);
 
-		[unroll]
-		for (uint texel_index = 0; texel_index < 16; ++texel_index)
+		texture_access.x = texture_access.x * width / 4096;
+		texture_access.y = texture_access.y * height / 4096;
+
+		uint2 range;
+		range.x = 1 + (width / 1024);
+		range.y = 1 + (height / 1024);
+
+		for (uint texel_index = 0; texel_index < (range.x * range.y); ++texel_index)
 		{
 			//Morton codes
 			uint i = (((texel_index >> 2) & 0x0007) & 0xFFFE) | (texel_index & 0x0001);
@@ -54,8 +58,8 @@ void build_hz(uint3 group : SV_GroupID, uint3 group_thread_id : SV_GroupThreadID
 			thread_texel.y = texture_access.y + j;
 
 			//Adjust to the current resolution, it is design for the worst case 4096x4096
-			thread_texel.x = thread_texel.x * width / 4096;
-			thread_texel.y = thread_texel.y * height / 4096;
+			thread_texel.x = thread_texel.x;
+			thread_texel.y = thread_texel.y;
 
 			thread_texel.x = min(thread_texel.x, width - 1);
 			thread_texel.y = min(thread_texel.y, height - 1);
@@ -64,13 +68,18 @@ void build_hz(uint3 group : SV_GroupID, uint3 group_thread_id : SV_GroupThreadID
 			min_value = min(min_value, value);
 		}
 
+		GroupMemoryBarrierWithGroupSync();
+
 		{
 			//Calculate the min value for all the mipmaps levels that is generating at the same time
 			uint value_before;
 			InterlockedMin(min_z_value_mip[mip0_offset + (quad.x / 2) + (quad.y / 2) * 8], asuint(min_value), value_before);
-			InterlockedMin(min_z_value_mip[mip1_offset + (quad.x / 4) + (quad.y / 4) * 4], asuint(min_value), value_before);
-			InterlockedMin(min_z_value_mip[mip2_offset + (quad.x / 8) + (quad.y / 8) * 2], asuint(min_value), value_before);
-			InterlockedMin(min_z_value_mip[mip3_offset], asuint(min_value), value_before);
+			uint value_before2;
+			InterlockedMin(min_z_value_mip[mip1_offset + (quad.x / 4) + (quad.y / 4) * 4], asuint(min_value), value_before2);
+			uint value_before3;
+			InterlockedMin(min_z_value_mip[mip2_offset + (quad.x / 8) + (quad.y / 8) * 2], asuint(min_value), value_before3);
+			uint value_before4;
+			InterlockedMin(min_z_value_mip[mip3_offset], asuint(min_value), value_before4);
 		}
 
 		GroupMemoryBarrierWithGroupSync();
@@ -83,7 +92,9 @@ void build_hz(uint3 group : SV_GroupID, uint3 group_thread_id : SV_GroupThreadID
 		//From 16x16 -> 8x8 the top mip
 		[flatten] if (thread_index < mip1_offset)
 		{
-			uint2 coords = uint2(thread_index % 8, thread_index / 8);
+			uint2 coords;
+			coords.x = (((thread_index >> 2) & 0x0007) & 0xFFFE) | (thread_index & 0x0001);
+			coords.y = ((thread_index >> 1) & 0x0003) | (((thread_index >> 3) & 0x0007) & 0xFFFC);
 			output_coords = group.xy * 8 + coords;
 			output_value = asfloat(min_z_value_mip[coords.x + coords.y * 8]);
 			write_out = true;
@@ -92,7 +103,9 @@ void build_hz(uint3 group : SV_GroupID, uint3 group_thread_id : SV_GroupThreadID
 		{
 			uint2 offset = uint2(512, 0);
 			uint local_thread_id = thread_index - mip1_offset;
-			uint2 coords = uint2(local_thread_id % 4, local_thread_id / 4);
+			uint2 coords;
+			coords.x = (((local_thread_id >> 2) & 0x0007) & 0xFFFE) | (local_thread_id & 0x0001);
+			coords.y = ((local_thread_id >> 1) & 0x0003) | (((local_thread_id >> 3) & 0x0007) & 0xFFFC);
 			output_coords = offset + group.xy * 4 + coords;
 			output_value = asfloat(min_z_value_mip[mip1_offset + coords.x + coords.y * 4]);
 			write_out = true;
@@ -101,7 +114,9 @@ void build_hz(uint3 group : SV_GroupID, uint3 group_thread_id : SV_GroupThreadID
 		{
 			uint2 offset = uint2(512, 256);
 			uint local_thread_id = thread_index - mip2_offset;
-			uint2 coords = uint2(local_thread_id % 2, local_thread_id / 2);
+			uint2 coords;
+			coords.x = (((local_thread_id >> 2) & 0x0007) & 0xFFFE) | (local_thread_id & 0x0001);
+			coords.y = ((local_thread_id >> 1) & 0x0003) | (((local_thread_id >> 3) & 0x0007) & 0xFFFC);
 			output_coords = offset + group.xy * 2 + coords;
 			output_value = asfloat(min_z_value_mip[mip2_offset + coords.x + coords.y * 2]);
 			write_out = true;
@@ -109,7 +124,6 @@ void build_hz(uint3 group : SV_GroupID, uint3 group_thread_id : SV_GroupThreadID
 		else [flatten] if (thread_index < mip_size)
 		{
 			uint2 offset = uint2(512, 256 + 128);
-			output_coords = offset + group.xy;
 			output_value = asfloat(min_z_value_mip[mip3_offset]);
 			write_out = true;
 		}
@@ -132,6 +146,7 @@ void build_hz(uint3 group : SV_GroupID, uint3 group_thread_id : SV_GroupThreadID
 
 	//The rest of the groups/threads can die here, only the last 16x16 threads of the last group will survive
 	if (group_dispatch_index < 64 * 64 - 1) return;
+
 
 	{
 		//Just leave the last group alive to create the last mips
@@ -191,9 +206,12 @@ void build_hz(uint3 group : SV_GroupID, uint3 group_thread_id : SV_GroupThreadID
 			//Use the group shared for building mip2, mip3, mip4, mip5
 			uint value_before;
 			InterlockedMin(min_z_value_mip[mip0_offset + (quad.x / 2) + (quad.y / 2) * 8], asuint(thread_local_mip1), value_before);
-			InterlockedMin(min_z_value_mip[mip1_offset + (quad.x / 4) + (quad.y / 4) * 4], asuint(thread_local_mip1), value_before);
-			InterlockedMin(min_z_value_mip[mip2_offset + (quad.x / 8) + (quad.y / 8) * 2], asuint(thread_local_mip1), value_before);
-			InterlockedMin(min_z_value_mip[mip3_offset], asuint(thread_local_mip1), value_before);
+			uint value_before2;
+			InterlockedMin(min_z_value_mip[mip1_offset + (quad.x / 4) + (quad.y / 4) * 4], asuint(thread_local_mip1), value_before2);
+			uint value_before3;
+			InterlockedMin(min_z_value_mip[mip2_offset + (quad.x / 8) + (quad.y / 8) * 2], asuint(thread_local_mip1), value_before3);
+			uint value_before4;
+			InterlockedMin(min_z_value_mip[mip3_offset], asuint(thread_local_mip1), value_before4);
 		}
 		
 		GroupMemoryBarrierWithGroupSync();
@@ -206,7 +224,9 @@ void build_hz(uint3 group : SV_GroupID, uint3 group_thread_id : SV_GroupThreadID
 		[flatten] if (thread_index < mip1_offset)
 		{
 			uint2 offset = uint2(512, 256 + 128 + 64 + 32 + 16);
-			uint2 coords = uint2(thread_index % 8, thread_index / 8);
+			uint2 coords;
+			coords.x = (((thread_index >> 2) & 0x0007) & 0xFFFE) | (thread_index & 0x0001);
+			coords.y = ((thread_index >> 1) & 0x0003) | (((thread_index >> 3) & 0x0007) & 0xFFFC);
 			output_coords = offset + coords;
 			output_value = asfloat(min_z_value_mip[coords.x + coords.y * 8]);
 			write_out = true;
@@ -216,7 +236,9 @@ void build_hz(uint3 group : SV_GroupID, uint3 group_thread_id : SV_GroupThreadID
 		{
 			uint2 offset = uint2(512, 256 + 128 + 64 + 32 + 16 + 8);
 			uint local_thread_id = thread_index - mip1_offset;
-			uint2 coords = uint2(local_thread_id % 4, local_thread_id / 4);
+			uint2 coords;
+			coords.x = (((local_thread_id >> 2) & 0x0007) & 0xFFFE) | (local_thread_id & 0x0001);
+			coords.y = ((local_thread_id >> 1) & 0x0003) | (((local_thread_id >> 3) & 0x0007) & 0xFFFC);
 			output_coords = offset + coords;
 			output_value = asfloat(min_z_value_mip[mip1_offset + coords.x + coords.y * 4]);
 			write_out = true;
@@ -226,7 +248,9 @@ void build_hz(uint3 group : SV_GroupID, uint3 group_thread_id : SV_GroupThreadID
 		{
 			uint2 offset = uint2(512, 256 + 128 + 64 + 32 + 16 + 8 + 4);
 			uint local_thread_id = thread_index - mip2_offset;
-			uint2 coords = uint2(local_thread_id % 2, local_thread_id / 2);
+			uint2 coords;
+			coords.x = (((local_thread_id >> 2) & 0x0007) & 0xFFFE) | (local_thread_id & 0x0001);
+			coords.y = ((local_thread_id >> 1) & 0x0003) | (((local_thread_id >> 3) & 0x0007) & 0xFFFC);
 			output_coords = offset + coords;
 			output_value = asfloat(min_z_value_mip[mip2_offset + coords.x + coords.y * 2]);
 			write_out = true;
